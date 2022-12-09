af75723ec8f0821a92ab2fb732f11eca5e5940fa
Emily Wang <egw7501@stern.nyu.edu>
2022-12-09 14:59:06 -0500

    fixed socket firing twice issue, fixed users array

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,4 +1,17 @@
-export default function Loading ({user}) {
+import React from "react";
+export default function Loading ({socket, user}) {
+    
+    //when both players in room are ready, twoready boolean will be set to true
+
+    let twoready = false;
+
+    React.useEffect(() => {
+        socket.on("ready_two", () => {
+            twoready = true;
+        })
+    }, []);
+
+
     return(
         <div>
             <img src = "https://i.imgur.com/00ZlPQ0.gif" />
